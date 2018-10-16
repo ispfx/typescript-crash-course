3d5a5617b2fe2d02bad96d5d85d4912fe9204fc8
kyleschaeffer <kyle@spirito.us>
2018-10-16 10:11:26 -0400

    Add interface

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+interface Product {
+  name: string;
+  price: number;
+  qty: number;
+  onSale?: boolean;
+  discounts?: string[];
+}
+
 // Our shopping cart
-const cart = [];
+const cart: Product[] = [];
 
 // Add a new product to the cart
-function add(name: string, price: number, qty: number, onSale: boolean = false): void {
-  cart.push({ name, price, qty, onSale });
+function add(product: Product): void {
+  cart.push(product);
 }
 
 // Get cart total
@@ -16,10 +24,28 @@ function total(): number {
 }
 
 // Add products to cart
-add('Apples', 149, 5);
-add('Oranges', 299, 3);
-add('Bananas', 99, 3);
-add('Chicken', 599, 2, true);
+add({
+  name: 'Apples',
+  price: 149,
+  qty: 5,
+});
+add({
+  name: 'Oranges',
+  price: 299,
+  qty: 3,
+});
+add({
+  name: 'Bananas',
+  price: 99,
+  qty: 3,
+});
+add({
+  name: 'Chicken',
+  price: 599,
+  qty: 2,
+  onSale: true,
+  discounts: [ '20% off', 'Coupon' ],
+});
 
 // Get cart total
 const cartTotal = total();
