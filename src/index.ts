0d78f0439c27d30ce7eee6a8a0a94c884e91c404
kyleschaeffer <kyle@spirito.us>
2018-10-16 11:04:44 -0400

    Add ProductType enum, add generics to Nutrition interface

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,24 @@ interface Product {
   qty: number;
   onSale?: boolean;
   discounts?: string[];
+  type?: ProductType;
+  nutrition?: Nutrition;
+}
+
+interface Nutrition {
+  calories: number;
+  isFrozen: boolean;
+  [index: string]: any;
+}
+
+enum ProductType {
+  Vegetable,
+  Dessert,
+  Fruit,
+  Poultry,
+  Fish,
+  Beef,
+  Pork,
 }
 
 // Our shopping cart
@@ -23,16 +41,34 @@ function total(): number {
   return sum;
 }
 
+// Get cart total calories
+function calories(): number {
+  let sum: number = 0;
+  cart.map((product) => {
+    if (product.nutrition) {
+      sum += product.nutrition.calories * product.qty;
+    }
+  });
+  return sum;
+}
+
 // Add products to cart
 add({
   name: 'Apples',
   price: 149,
   qty: 5,
+  type: ProductType.Fruit,
+  nutrition: {
+    calories: 140,
+    isFrozen: false,
+    vitaminC: 14,
+  },
 });
 add({
   name: 'Oranges',
   price: 299,
   qty: 3,
+  type: ProductType.Fruit,
 });
 add({
   name: 'Bananas',
@@ -45,6 +81,11 @@ add({
   qty: 2,
   onSale: true,
   discounts: [ '20% off', 'Coupon' ],
+  type: ProductType.Poultry,
+  nutrition: {
+    calories: 250,
+    isFrozen: true,
+  },
 });
 
 // Get cart total
